375d1e8c880432ed0d194b84d3ca562ab3ea65d4
chandershekhar22 <chandersj.it.22@nitj.ac.in>
2025-06-28 21:21:00 +0530

    done

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND_URL, // ✅ Now environment-based
+baseURL: import.meta.env.VITE_BACKEND_URL
+ // ✅ Now environment-based
 });
 
 axiosInstance.interceptors.request.use(
